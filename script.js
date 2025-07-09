6ad493c5442d721275f81f425068b84d7517f04a
poorvakhatri04 <146342264+poorvakhatri04@users.noreply.github.com>
2025-07-09 22:26:30 +0530

    password_generator

--- script.js
+++ script.js
@@ -1,19 +1,51 @@
-let time=document.getElementById("time");
-let timeformat=document.getElementById("timeformat");
-document.addEventListener("DOMContentLoaded",()=>{
-    setInterval(showTime,1000);
+let inputSlider=document.getElementById("inputSlider");
+let sliderValue=document.getElementById("sliderValues");
+let passBox=document.getElementById("passBox");
+let lowercase=document.getElementById("lowercase");
+let uppercase=document.getElementById("uppercase");
+let numbers=document.getElementById("numbers");
+let symbols=document.getElementById("symbols");
+let gnbtn=document.getElementById("gnbtn");
+let copyIcon=document.getElementById("copyIcon");
+
+//showing input slider value
+sliderValue.textContent=inputSlider.value;
+inputSlider.addEventListener('input',()=>{
+    sliderValue.textContent=inputSlider.value;
 });
-const showTime=()=>{
-    let date=new Date();
-    let hr=date.getHours();
-    let min=date.getMinutes();
-    let secs=date.getSeconds();
-    hr=hr<10?`0${hr}`:hr;
-    min=min<10?`0${min}`:min;
-    secs=secs<10?`0${secs}`:secs;
 
-    time.innerHTML=`${hr} : ${min} : ${secs}`;
+gnbtn.addEventListener('click',()=>{
+    passBox.value=generatePassword();
+});
 
-    timeformat.innerHTML=hr>12?"PM":"AM";
-    //console.log(hr+":"+min+":"+secs)
+let upperChars="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowerChars="abcdefghijklmnopqrstuvwxyz";
+let Numbers="0123456789";
+let Symbols="!@#$%^&*_~"
+//function to generate password
+function generatePassword(){
+    let genPassword="";
+    let allChars="";
+    allChars+=lowercase.checked ? lowerChars:"";
+    allChars+=uppercase.checked ? upperChars:"";
+    allChars+=numbers.checked ? Numbers:"";
+    allChars+=symbols.checked ? Symbols:"";
+
+    let i=1;
+    while(i<=inputSlider.value){
+        genPassword+= allChars.charAt(Math.floor(Math.random()*allChars.length));
+        i++;
+    }
+    return genPassword;
 }
+copyIcon.addEventListener('click',()=>{
+    if(passBox.value!=""||passBox.value.length>=1){
+       navigator.clipboard.writeText(passBox.value);
+       copyIcon.innerText="check";
+       copyIcon.title="Password Copied";
+       setTimeout(()=>{
+        copyIcon.innerHTML="content_copy";
+        copyIcon.title="";
+       },3000)
+    }
+});
